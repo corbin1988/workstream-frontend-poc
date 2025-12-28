e2da5ae455b1dbf3f1a29e0da59058a6c1912cf2
Kyle <kyle.c.hurst@gmail.com>
2025-12-28 12:15:38 -0500

    Add flowbite dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,18 @@ export default function Document() {
     <Html lang="en" suppressHydrationWarning>
       <Head>
         <ThemeModeScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              // On page load or when changing themes, best to add inline in head to avoid FOUC
+              if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+              } else {
+                document.documentElement.classList.remove('dark');
+              }
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
