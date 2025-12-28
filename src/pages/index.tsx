55429f22334197753e909a0921048a603eeb725e
Kyle <kyle.c.hurst@gmail.com>
2025-12-28 10:50:53 -0500

    Add flowbite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "flowbite-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +47,7 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+          <Button className="bg-red-500 hover:bg-red-600">Custom Button</Button>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
